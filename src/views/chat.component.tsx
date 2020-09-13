65f2ac6ba13cc17ddf2ae34ae51ce8aca97c9712
John Hershberg <johnher@gmail.com>
2020-09-12 21:19:53 -0700

    Added Bootstrap

--- src/views/chat.component.tsx
+++ src/views/chat.component.tsx
@@ -1,9 +1,24 @@
 import React from "react";
+import InputGroup from "react-bootstrap/InputGroup";
+import FormControl from "react-bootstrap/FormControl";
+import Button from "react-bootstrap/Button";
 
 import "./chat.styles.scss";
 
 const ChatView = () => {
-  return <div className="chatViewOuterContainer"></div>;
+  return (
+    <div className="chatViewOuterContainer">
+      <div className="messagesContainer"></div>
+      <div className="inputContainer">
+        <InputGroup className="mb-0">
+          <FormControl placeholder="message" aria-label="message" aria-describedby="basic-addon2" />
+          <InputGroup.Append>
+            <Button variant="outline-secondary">Send</Button>
+          </InputGroup.Append>
+        </InputGroup>
+      </div>
+    </div>
+  );
 };
 
 export default ChatView;
