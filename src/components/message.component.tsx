b1e595d3e667d54287e8259c85bee34540227ebf
John Hershberg <johnher@gmail.com>
2020-09-16 13:57:56 -0700

    Scrolling messages

--- src/components/message.component.tsx
+++ src/components/message.component.tsx
@@ -1,10 +1,20 @@
 import React, { FunctionComponent } from "react";
 import "./message.styles.scss";
 
-interface MessageProps {}
+interface MessageProps {
+  from: string;
+  message: string;
+}
 
-const Message: FunctionComponent<MessageProps> = () => {
-  return <div className="messageContainer"></div>;
+const Message: FunctionComponent<MessageProps> = ({ from, message }) => {
+  return (
+    <div className="messageContainer">
+      <div className="textContainer">
+        <div className="topInfoContainer">{from}</div>
+        {message}
+      </div>
+    </div>
+  );
 };
 
 export default Message;
--- src/components/messageLog.component.tsx
+++ src/components/messageLog.component.tsx
@@ -4,12 +4,19 @@ import "./messageLog.styles.scss";
 
 interface MessageLogProps {}
 
+const tempMessage =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+const tempShortMessage =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+
 const MessageLog: FunctionComponent<MessageLogProps> = () => {
   return (
     <div className="messageLog">
-      <Message />
-      <Message />
-      <Message />
+      <div className="messagesOverflow">
+        <Message from={"Alice"} message={tempMessage} />
+        <Message from={"Bob"} message={tempShortMessage} />
+        <Message from={"Chris"} message={tempMessage} />
+      </div>
     </div>
   );
 };
