1cb59e6555b89daef3ec0b08349b1eefcd0303c4
John Hershberg <johnher@gmail.com>
2020-09-18 21:09:46 -0700

    Added gradients

--- src/components/message.component.tsx
+++ src/components/message.component.tsx
@@ -10,7 +10,7 @@ interface MessageProps {
 const Message: FunctionComponent<MessageProps> = ({ from, message, activeUser }) => {
   return (
     <div className={"messageContainer " + (from === activeUser ? "messageFromSelf" : null)}>
-      <div className="textContainer">
+      <div className="textContainer neu-small">
         <div className="topInfoContainer">{from}</div>
         <div className="messageBody">{message}</div>
       </div>
