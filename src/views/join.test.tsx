b8b1b6d4ba6d2d9e0bac3f6ef270a5da7fe04249
John Hershberg <johnher@gmail.com>
2020-09-27 17:20:33 -0700

    Moved re-used consts outside of the test

--- src/views/join.test.tsx
+++ src/views/join.test.tsx
@@ -7,10 +7,10 @@ jest.mock("../serverRoutes.ts");
 
 describe("Joining the chat", () => {
   const wrapper = shallow(<Join activeUserName="" setActiveUserName={() => {}} />);
+  const mockEvent = { preventDefault: jest.fn() }; // This is needed because the button sends an empty event that doesn't have the preventDefault function
+  const flushPromises = () => new Promise(setImmediate); // Using this to resolve all promises
 
   it("should not have a redirect component if username is unavailable", async () => {
-    const mockEvent = { preventDefault: jest.fn() }; // This is needed because the button sends an empty event that doesn't have the preventDefault function
-    const flushPromises = () => new Promise(setImmediate); // Using this to resolve all promises
     wrapper.setProps({ activeUserName: "nameUnAvailable" });
     wrapper.find("Button").simulate("click", mockEvent);
     await flushPromises();
@@ -20,8 +20,6 @@ describe("Joining the chat", () => {
   });
 
   it("should have redirect component if username is available", async () => {
-    const mockEvent = { preventDefault: jest.fn() }; // This is needed because the button sends an empty event that doesn't have the preventDefault function
-    const flushPromises = () => new Promise(setImmediate); // Using this to resolve all promises
     wrapper.setProps({ activeUserName: "nameAvailable" });
     wrapper.find("Button").simulate("click", mockEvent);
     await flushPromises();
