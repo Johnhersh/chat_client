42e8fe879cba133b752514b96555cf9dfa1e85ff
John Hershberg <johnher@gmail.com>
2020-09-30 16:44:12 -0700

    Added mock messages

--- src/__mocks__/serverRoutes.ts
+++ src/__mocks__/serverRoutes.ts
@@ -1,5 +1,20 @@
+import { dbMessage } from "../serverRoutes";
+
 export async function getMessageLog() {
-  return Promise.resolve();
+  console.log(`In mocked getMessageLog`);
+  const message1: dbMessage = {
+    id: 1,
+    chat_message: "Test message 1",
+    from_user: "admin1",
+    time: "now",
+  };
+  const message2: dbMessage = {
+    id: 2,
+    chat_message: "Test message 2",
+    from_user: "admin2",
+    time: "now",
+  };
+  return Promise.resolve([message1, message2]);
 }
 
 export async function logIn(newUserName: string) {
