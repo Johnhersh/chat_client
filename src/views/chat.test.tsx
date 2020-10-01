1d2d489d9efa55b2040d0804879deb645e1cbec7
John Hershberg <johnher@gmail.com>
2020-09-30 20:47:04 -0700

    Checking join socket functionality

--- src/views/chat.test.tsx
+++ src/views/chat.test.tsx
@@ -2,11 +2,20 @@ import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import { act } from "react-dom/test-utils";
 import userEvent from "@testing-library/user-event";
-import SocketMock from "socket.io-mock";
+import io from "socket.io-client";
+// import SocketMock from "socket.io-mock";
 
 import ChatView from "./chat.component";
 
 jest.mock("../serverRoutes.ts");
+jest.mock("socket.io-client", () => {
+  const emit = jest.fn();
+  const on = jest.fn();
+  const off = jest.fn();
+  const close = jest.fn();
+  const socket = { emit, on, off, close };
+  return jest.fn(() => socket);
+});
 
 const setup = () => {
   const utils = render(<ChatView activeUserName="nameAvailable" />);
@@ -19,7 +28,7 @@ const setup = () => {
   };
 };
 
-describe("sending a message", () => {
+/*describe("sending a message", () => {
   it("should reset the input box when pressing Enter", async () => {
     const testMessage = "Test Message!";
     const { input, unmount } = setup();
@@ -50,6 +59,27 @@ describe("sending a message", () => {
     });
 
     // screen.debug();
+
+    unmount();
+  });
+});*/
+
+describe("socket.io functionality", () => {
+  it("should receive a 'join' message with room and username information ", async () => {
+    const { unmount } = setup();
+    const ENDPOINT = "http://localhost:3001";
+    const mockSocket = io(ENDPOINT);
+
+    await waitFor(() => {}); // This is needed because a useEffect has an async call that updates state
+
+    expect(mockSocket.emit).toHaveBeenCalledWith(
+      "join",
+      {
+        room: "general",
+        username: "nameAvailable",
+      },
+      expect.any(Function)
+    );
 
     unmount();
   });
