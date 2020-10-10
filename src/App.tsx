6c18da527aa35a7b0bd6369f427ad8b7c04e48f4
John Hershberg <johnher@gmail.com>
2020-10-10 15:44:21 -0700

    Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import ChatView from "./views/chat.component";
 import Join from "./views/join.component";
-import {UsernameContext} from "./Context";
+import { UsernameContext } from "./Context";
 import "./App.scss";
 import "./global.styles.scss";
 
@@ -13,13 +13,13 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <UsernameContext.Provider value={{activeUsername: username, setActiveUsername: setUsername}}>
-        <Route path="/" exact>
-          <Join/>
-        </Route>
-        <Route path="/chat" exact>
-          <ChatView activeUserName={username} />
-        </Route>
+        <UsernameContext.Provider value={{ activeUsername: username, setActiveUsername: setUsername }}>
+          <Route path="/" exact>
+            <Join />
+          </Route>
+          <Route path="/chat" exact>
+            <ChatView activeUserName={username} />
+          </Route>
         </UsernameContext.Provider>
       </Router>
     </div>
